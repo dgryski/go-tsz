c5e7053547e1915deaa7a74de05365cfea0e0099
woodsaj <anthony@monkey.id.au>
2015-11-06 15:05:46 +0800

    refactor concurrency unit tests.

--- tsz_test.go
+++ tsz_test.go
@@ -1,8 +1,6 @@
 package tsz
 
 import (
-	"io"
-	"sync"
 	"testing"
 	"time"
 )
@@ -140,68 +138,6 @@ func TestRoundtrip(t *testing.T) {
 	}
 }
 
-func writer(s *Series, done chan struct{}, sleep time.Duration) {
-	for _, p := range TwoHoursData {
-		s.Push(p.t, p.v)
-		time.Sleep(sleep)
-	}
-	close(done)
-}
-
-func reader(s *Series, done chan struct{}, errors chan string, wg *sync.WaitGroup, t *testing.T) {
-	// continuously create iterators to verify the data
-	// * when we know the writer is done, we make sure
-	// we do 1 last pass verifying all data
-	// * until then, we only require data to be correct up until
-	// there's no more data returned,and don't require all data to be returned
-	lastPass := false
-	for {
-		select {
-		case <-done:
-			lastPass = true
-		default:
-		}
-		it := s.Iter()
-		iters := 0
-		for _, w := range TwoHoursData {
-			iters += 1
-			if it.Next() {
-				tt, vv := it.Values()
-				if w.t != tt || w.v != vv {
-					errors <- fmt.Sprintf("Values()=(%v,%v), want (%v,%v)\n", tt, vv, w.t, w.v)
-				}
-			} else {
-				if lastPass {
-					errors <- fmt.Sprintf("Next()=false, want true")
-				}
-				// if we're not the last pass, it's ok to not have all data yet, stop looking
-				break
-			}
-		}
-
-		// if not in last pass, Next() may suddenly have a new value now, or not..
-		if lastPass && it.Next() {
-			errors <- fmt.Sprintf("Next()=true, want false")
-		}
-
-		// if not a single value was written yet, we expect an EOF error
-		if iters == 0 {
-			if err := it.Err(); err != io.EOF {
-				errors <- fmt.Sprintf("it.Err()=%v, want EOF", err)
-			}
-			// but if one or more values exist, we should have a clean end of record
-		} else {
-			if err := it.Err(); err != nil {
-				errors <- fmt.Sprintf("it.Err()=%v, want nil. after %d iters", err, iters)
-			}
-		}
-		if lastPass {
-			break
-		}
-	}
-	wg.Done()
-}
-
 func TestConcurrentRoundtripImmediateWrites(t *testing.T) {
 	testConcurrentRoundtrip(t, time.Duration(0))
 }
@@ -218,25 +154,59 @@ func TestConcurrentRoundtrip10MsBetweenWrites(t *testing.T) {
 func testConcurrentRoundtrip(t *testing.T, sleep time.Duration) {
 
 	s := New(TwoHoursData[0].t)
+	//notify the reader about the number of writes that have been sent.
+	writeNotify := make(chan int)
 	done := make(chan struct{})
-	errors := make(chan string)
-
-	wg := &sync.WaitGroup{}
-
-	go func() {
-		for err := range errors {
-			t.Error(err)
+	// continuously iterate over the values of the series.
+	// when a write is made, the total number of points in the series
+	// will be sent over the channel, so we can make sure we are reading
+	// the correct amount of values.
+	go func(numPoints chan int, finished chan struct{}) {
+		n := 0
+		for {
+			select {
+			case n = <-numPoints:
+				//t.Logf("new point written")
+			default:
+				readCount := 0
+				it := s.Iter()
+				for it.Next() == true {
+					tt, vv := it.Values()
+					expectedT := TwoHoursData[readCount].t
+					expectedV := TwoHoursData[readCount].v
+					if expectedT != tt || expectedV != vv {
+						t.Errorf("thawed metric values dont match what was written. (%d, %f) != (%d, %f)\n", tt, vv, expectedT, expectedV)
+					}
+					readCount++
+				}
+				if readCount != n {
+					t.Logf("Checking if point was written while we were reading.")
+					// check if a point was written while we were running
+					select {
+					case n = <-numPoints:
+						t.Logf("new point written")
+						if readCount != n {
+							t.Errorf("expexcted %d values in series, got %d", n, readCount)
+						}
+					default:
+						t.Errorf("expexcted %d values in series, got %d", n, readCount)
+					}
+				}
+			}
+			select {
+			case <-finished:
+				return
+			default:
+			}
 		}
-	}()
+	}(writeNotify, done)
 
-	for i := 0; i < 10; i++ {
-		wg.Add(1)
-		go reader(s, done, errors, wg, t)
+	for i := 0; i < 100; i++ {
+		s.Push(TwoHoursData[i].t, TwoHoursData[i].v)
+		writeNotify <- i + 1
+		time.Sleep(sleep)
 	}
-
-	go writer(s, done, sleep)
-
-	wg.Wait()
+	done <- struct{}{}
 }
 
 func BenchmarkEncode(b *testing.B) {
